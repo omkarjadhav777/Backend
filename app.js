8026c71b4a8e8c473f5d934ffd6d10748ced78a1
omkarjadhav777 <148333479+omkarjadhav777@users.noreply.github.com>
2025-06-30 23:56:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var requestLogger = require('./helper/requestLogger')
 var apiAuth = require('./helper/apiAuthentication')
 var cors = require('cors')
 
-const path = require('path');
 dotenv.config()
 
 var usersRouter = require('./routes/userRouter')
@@ -18,15 +17,8 @@ app.use(express.json())
 app.use(requestLogger)
 
 app.use('/api/users', usersRouter)
-app.use('/api/group', apiAuth.validateToken,gorupRouter)
-app.use('/api/expense', apiAuth.validateToken,expenseRouter)
-
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    });
-   }
+app.use('/api/group', apiAuth.validateToken, gorupRouter)
+app.use('/api/expense', apiAuth.validateToken, expenseRouter)
 
 //To detect and log invalid api hits 
 app.all('*', (req, res) => {
@@ -34,7 +26,7 @@ app.all('*', (req, res) => {
     res.status(404).json({
         status: 'fail',
         message: 'Invalid path'
-      })
+    })
 })
 
 const port = process.env.PORT || 3001
