6fea323f26fa83a969dd9c5db072bf520a47ae65
omkarjadhav777 <148333479+omkarjadhav777@users.noreply.github.com>
2025-06-30 23:44:07 +0530

    Update logger.js

--- helper/logger.js
+++ helper/logger.js
@@ -8,18 +8,10 @@ const customFormat = format.combine(format.timestamp(), format.printf((log) => {
     return `${log.timestamp} | ${log.level.toUpperCase().padEnd(7)}| ${log.message}`
 }))
 
-
 const logger = createLogger({
     format: customFormat,
     transports: [
-        new transports.File({
-            filename: "./logs/app.log",
-            level: 'info'
-        }),
-        new transports.File({
-            filename: "./logs/error.log",
-            level: 'error'
-        })
+        new transports.Console()
     ]
 })
 
